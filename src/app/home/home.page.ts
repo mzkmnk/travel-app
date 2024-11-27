5e0272337e84561ce3ce09472d17bfd65adc4a84
mzkmnk <sma0729mex@gmail.com>
2024-11-27 22:09:00 +0900

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class HomePage {
   constructor() {}
