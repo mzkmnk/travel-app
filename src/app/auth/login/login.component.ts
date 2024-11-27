016f204da9e0e36a6875ce70bd68e119c7975ad5
mzkmnk <sma0729mex@gmail.com>
2024-11-28 07:56:42 +0900

    wip:login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,31 @@
 import {Component} from "@angular/core";
+import {IonButton} from "@ionic/angular/standalone";
+import {NgOptimizedImage} from "@angular/common";
+import {createClient, SupabaseClient} from "@supabase/supabase-js";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: "app-login",
-  template:`
-    <div>
-      <p>Login</p>
+  imports: [
+    IonButton,
+    NgOptimizedImage
+  ],
+  template: `
+    <div class="flex items-center justify-center h-screen container">
+      <ion-button>
+        <div class="flex items-center justify-center gap-2">
+          <img ngSrc="assets/icon/GoogleIcon.svg" width="20" height="20" alt="google icon">
+          <p>Login with Google</p>
+        </div>
+      </ion-button>
     </div>
   `
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  private readonly supabase:SupabaseClient;
+
+  constructor() {
+    this.supabase =  createClient(environment.supabaseUrl,environment.supabaseKey);
+  }
+}
