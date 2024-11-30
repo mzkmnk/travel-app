3ffe61e55f0d472959ff3ec9cb6f08d1de1a8ff5
mzkmnk <sma0729mex@gmail.com>
2024-11-30 23:49:59 +0900

    fix:guard修正

--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -1,26 +1,25 @@
-import {CanActivateFn, Router} from "@angular/router";
+import {CanActivateFn, Router, UrlTree} from "@angular/router";
 import {inject} from "@angular/core";
-import {SupabaseService} from "@/src/shared/services/supabase.service";
-import { map, take} from "rxjs";
 import {ToastService} from "@/src/shared/services/toast.service";
-import {UserSignalStore} from "@/src/shared/stores/user.signal-store";
+import {AuthSignalStore} from "@/src/shared/stores/auth.signal-store";
+import {toObservable} from "@angular/core/rxjs-interop";
+import {filter, map} from "rxjs";
 
+/** 認証ガード */
 export const AuthGuard:CanActivateFn = () => {
-  const supabaseService = inject(SupabaseService);
   const toastService = inject(ToastService);
   const router = inject(Router);
-  const userSignalStore = inject(UserSignalStore);
 
+  const authSignalStore = inject(AuthSignalStore);
 
-  return supabaseService.getCurrentUser().pipe(
-    take(1),
-    map((isAuthenticated) => {
-      console.log('ok',isAuthenticated);
-      if(!isAuthenticated){
+  return toObservable(authSignalStore.user).pipe(
+    filter(isAuth => isAuth !== undefined),
+    map((isAuth):true|UrlTree => {
+      if(!isAuth){
         toastService.presentToast({toastOptions:{message:'再ログインをお願いします',color:'warning',position:'top',duration:3000}}).then();
-        return router.parseUrl('/auth/login');
+        return router.createUrlTree(['/auth'])
       }
-      return true
+      return true;
     })
   )
 };
